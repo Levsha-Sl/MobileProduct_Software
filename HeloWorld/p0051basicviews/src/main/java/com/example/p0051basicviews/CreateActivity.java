7fe1b72535d038ab8c7010f018243c6a99edf377
Levsha-Sl <69717693+Levsha-Sl@users.noreply.github.com>
2020-11-17 14:05:32 +0700

    lab1

---
+++ HeloWorld/p0051basicviews/src/main/java/com/example/p0051basicviews/CreateActivity.java
@@ -0,0 +1,14 @@
+package com.example.p0051basicviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myscreen);
+    }
+}
