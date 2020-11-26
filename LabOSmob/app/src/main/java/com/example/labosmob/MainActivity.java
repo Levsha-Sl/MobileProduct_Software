dc523f8593c015bcdeef076df4e0cd15de48a9b0
Levsha-Sl <69717693+Levsha-Sl@users.noreply.github.com>
2020-11-26 21:22:27 +0700

    2.4

--- LabOSmob/app/src/main/java/com/example/labosmob/MainActivity.java
+++ LabOSmob/app/src/main/java/com/example/labosmob/MainActivity.java
@@ -2,16 +2,33 @@ package com.example.labosmob;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView TOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity);
-        TextView newText = (TextView) findViewById(R.id.textOutput);
-        String S = newText.getText() + " " + Phraser.PhraserGen();
-        newText.setText(S);
+
+        TOutput = (TextView) findViewById(R.id.textOutput);
+        Button newButton = (Button) findViewById(R.id.gen);
+
+        String S = TOutput.getText() + " " + Phraser.PhraserGen();
+        TOutput.setText(S);
+
+        View.OnClickListener oclBtnGen = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String S = getString(R.string.textOutput) + " " + Phraser.PhraserGen();
+                TOutput.setText(S);
+            }
+        };
+
+        newButton.setOnClickListener(oclBtnGen);
     }
 }
