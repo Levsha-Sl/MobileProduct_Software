c1095d34d7e50072b2adc92114c9bee7c079801c
Levsha-Sl <69717693+Levsha-Sl@users.noreply.github.com>
2020-12-12 21:41:04 +0700

    added initRecyclerView

--- LabOSmob/RecyclerView/src/main/java/com/example/recyclerview2/MainActivity.java
+++ LabOSmob/RecyclerView/src/main/java/com/example/recyclerview2/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.recyclerview2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView List;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initList();
+    }
+
+    ///получение RecyclerView
+    private void initList(){
+        List = findViewById(R.id.list);
+        // будем отображать элементы линейно
+        List.setLayoutManager(new LinearLayoutManager(this));
     }
 }
