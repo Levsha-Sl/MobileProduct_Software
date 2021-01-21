40b2a76b79152cb76c1fb9bfaf4857fc298195b8
Levsha-Sl <69717693+Levsha-Sl@users.noreply.github.com>
2021-01-21 21:04:07 +0700

    5.4b added void HandleTouch

--- Emptyactivity/app/src/main/java/a/empty_activity/MainActivity.java
+++ Emptyactivity/app/src/main/java/a/empty_activity/MainActivity.java
@@ -2,9 +2,11 @@ package a.empty_activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -55,24 +57,76 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ButtonClick(View v) {
-        v.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v) {
-                        TextView statusText =
-                                findViewById(R.id.textClick);
-                        statusText.setText(R.string.buttonClick_text0);
-                    }
-                }
-        );
-        v.setOnLongClickListener(
-                new Button.OnLongClickListener() {
-                    public boolean onLongClick(View v) {
-                        TextView statusText =
-                                findViewById(R.id.textClick);
-                        statusText.setText(R.string.buttonClick_text1);
-                        return false;
-                    }
-                }
-        );
+        v.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                buttonClick(R.string.buttonClick_text0);
+            }
+        });
+        v.setOnLongClickListener(new Button.OnLongClickListener() {
+            public boolean onLongClick(View v) {
+                buttonClick(R.string.buttonClick_text1);
+                return false;
+            }
+        });
+    }
+
+
+
+    public void TouchLayout(View v) {
+        v.setOnTouchListener(new ConstraintLayout.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent m) {
+                handleTouch(m);
+                return true;
+            }
+        });
+    }
+
+    void buttonClick(int string) {
+        TextView statusText = findViewById(R.id.textClick);
+        statusText.setText(string);
+    }
+    void handleTouch(MotionEvent m){
+        TextView textView1 = findViewById(R.id.textViewME);
+        TextView textView2 = findViewById(R.id.textViewME2);
+
+        int pointerCount = m.getPointerCount();
+
+        for (int i = 0; i < pointerCount; i++)
+        {
+            int x = (int) m.getX(i);
+            int y = (int) m.getY(i);
+            int id = m.getPointerId(i);
+            int action = m.getActionMasked();
+            int actionIndex = m.getActionIndex();
+            String actionString;
+
+            switch (action)
+            {
+                case MotionEvent.ACTION_DOWN:
+                    actionString = "DOWN";
+                    break;
+                case MotionEvent.ACTION_UP:
+                    actionString = "UP";
+                    break;
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    actionString = "PNTR DOWN";
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                    actionString = "PNTR UP";
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    actionString = "MOVE";
+                    break;
+                default:
+                    actionString = "";
+            }
+
+            String touchStatus = "Action: " + actionString + " Index: " +
+                    actionIndex + " ID: " + id + " X: " + x + " Y: " + y;
+            if (id == 0)
+                textView1.setText(touchStatus);
+            else
+                textView2.setText(touchStatus);
+        }
     }
 }
